de081c692e02ecbc8cb062ff87ad0e5acbdd5d2f
falintino <falintino10@gmail.com>
2026-07-19 21:16:15 +0700

    Fix video thumbnail

--- data/videos.ts
+++ data/videos.ts
@@ -2,7 +2,7 @@ export const videos = [
   {
     id: 1,
     title: "Di Balik Layar",
-    thumbnail: "/images/releases/di-balik-layar.jpg",
+    thumbnail: "/images/releases/di-balik-layar.jpeg",
     url: "https://www.youtube.com/watch?v=dPmAs3-j9o8&loop=0",
     platform: "YouTube",
     duration: "3:42",
